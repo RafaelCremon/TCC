ea051de7372fef56f986ef44dfd5b60c95ede48a
guique1 <gustavohrm2018@gmail.com>
2025-05-27 22:19:09 -0300

    mapa atualizado denovo

--- js/mapa.js
+++ js/mapa.js
@@ -6,59 +6,63 @@ const viewer = new Marzipano.Viewer(document.getElementById('pano'), {
 });
 
 const panoData = [
-  { name: "Catraca", image: "../assets/minimapa/CATRACA.jpg", yaw: Math.PI / 1 },
-  { name: "Escadaria", image: "../assets/minimapa/DOURADO_ESCADARIA.jpg", yaw: 10 },
-  { name: "Safe_zone", image: "../assets/minimapa/SAFE_ZONE.jpg", yaw: -Math.PI / 2 },
-  { name: "hell", image: "../assets/minimapa/HELL.jpg", yaw: Math.PI },
-  { name: "centro_patio", image: "../assets/minimapa/CENTRO_PATIOO.jpg", yaw: 1 },
-  { name: "Bom_Gosto", image: "../assets/minimapa/BOM_GOSTO.jpg", yaw: 0.5 },
-  { name: "impressao", image: "../assets/minimapa/FUNDO_IMPRESSAO.jpg", yaw: 1.5 },
-  { name: "elevadores", image: "../assets/minimapa/ELEVADORES.jpg", yaw: 2 },
-  { name: "fundo_corredor", image: "../assets/minimapa/FUNDO_CORREDOR.jpg", yaw: -1 },
-  { name: "transporte", image: "../assets/minimapa/TRANSPORTE.jpg", yaw: -Math.PI },
-  { name: "escadaria_principal", image: "../assets/minimapa/ESCADAS_PRINCIPAL.jpg", yaw: 0 },
-  { name: "secretaria", image: "../assets/minimapa/SECRETARIA.jpg", yaw: 0 },
-  { name: "dema", image: "../assets/minimapa/DEMA.jpg", yaw: 0 }
+  { name: "Catraca", image: "../assets/minimapa/CATRACA.jpg", initialYaw: 180 },
+  { name: "Escadaria", image: "../assets/minimapa/DOURADO_ESCADARIA.jpg", initialYaw: Math.PI },
+  { name: "Safe_zone", image: "../assets/minimapa/SAFE_ZONE.jpg", initialYaw: 0 },
+  { name: "hell", image: "../assets/minimapa/HELL.jpg", initialYaw: 0 },
+  { name: "centro_patio", image: "../assets/minimapa/CENTRO_PATIOO.jpg", initialYaw: 0 },
+  { name: "Bom_Gosto", image: "../assets/minimapa/BOM_GOSTO.jpg", initialYaw: 0 },
+  { name: "impressao", image: "../assets/minimapa/FUNDO_IMPRESSAO.jpg", initialYaw: 0 },
+  { name: "elevadores", image: "../assets/minimapa/ELEVADORES.jpg", initialYaw: 0 },
+  { name: "fundo_corredor", image: "../assets/minimapa/FUNDO_CORREDOR.jpg", initialYaw: 0 },
+  { name: "transporte", image: "../assets/minimapa/TRANSPORTE.jpg", initialYaw: 0 },
+  { name: "escadaria_principal", image: "../assets/minimapa/ESCADAS_PRINCIPAL.jpg", initialYaw: 0 },
+  { name: "secretaria", image: "../assets/minimapa/SECRETARIA.jpg", initialYaw: 0 },
+  { name: "dema", image: "../assets/minimapa/DEMA.jpg", initialYaw: 0 }
 ];
 
 const scenes = panoData.map((data, index) => {
   const source = Marzipano.ImageUrlSource.fromString(data.image);
   const geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
-  const limiter = Marzipano.RectilinearView.limit.traditional(
-    2048,
-    100 * Math.PI / 180,
-    120 * Math.PI / 180
+  const limiter = Marzipano.RectilinearView.limit.traditional(2048, 100 * Math.PI / 180, 120 * Math.PI / 180);
+
+  const view = new Marzipano.RectilinearView(
+    { yaw: data.initialYaw || 0 },
+    limiter
   );
 
-  const initialViewParams = {
-    yaw: data.yaw || 0, // valor padrão se não for definido
-    pitch: data.pitch || 0,
-    fov: data.fov || Math.PI / 2
-  };
-
-  const view = new Marzipano.RectilinearView(initialViewParams, limiter);
   const scene = viewer.createScene({ source, geometry, view });
 
-  // Hotspot Avançar
+  // Hotspot para próxima cena
   if (index < panoData.length - 1) {
     const nextHotspot = document.createElement('div');
     nextHotspot.className = 'hotspot arrow next';
     nextHotspot.title = "Próxima";
+
     nextHotspot.addEventListener('click', () => {
       scenes[index + 1].scene.switchTo();
     });
-    scene.hotspotContainer().createHotspot(nextHotspot, { yaw: 1.0, pitch: 0 });
+
+    scene.hotspotContainer().createHotspot(nextHotspot, {
+      yaw: (data.initialYaw || 0),
+      pitch: 0
+    });
   }
 
-  // Hotspot Voltar
+  // Hotspot para cena anterior
   if (index > 0) {
     const prevHotspot = document.createElement('div');
     prevHotspot.className = 'hotspot arrow prev';
     prevHotspot.title = "Voltar";
+
     prevHotspot.addEventListener('click', () => {
       scenes[index - 1].scene.switchTo();
     });
-    scene.hotspotContainer().createHotspot(prevHotspot, { yaw: -1.0, pitch: 0 });
+
+    scene.hotspotContainer().createHotspot(prevHotspot, {
+      yaw: (data.initialYaw || 0) + Math.PI,
+      pitch: 0
+    });
   }
 
   return {
@@ -67,4 +71,5 @@ const scenes = panoData.map((data, index) => {
   };
 });
 
+// Mostra a primeira cena
 scenes[0].scene.switchTo();
